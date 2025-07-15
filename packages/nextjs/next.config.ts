8d40f3bec20fc4c41fec500c10aacbf2614096b6
fjkang🎶 <oncwnuPp8MBrxOOqevGbwu-wtGhM@git.weixin.qq.com>
2025-07-15 09:50:18 +0800

    Add the configuration of images.remotePatterns to load my page avatar

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -17,6 +17,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   webpack: config => {
